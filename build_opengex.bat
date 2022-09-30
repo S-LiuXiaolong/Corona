2a65cfca818768fad58f058d94ab4b389d254856
ShepherdOuO <2833244138@qq.com>
2022-10-01 00:16:12 +0800

    Update CMakeLists

--- build_opengex.bat
+++ build_opengex.bat
@@ -3,6 +3,6 @@ git submodule update --init External\src\OpenGEX
 mkdir External\build\OpenGEX
 pushd External\build\OpenGEX
 cmake -S ..\..\src\OpenGEX -B .
-cmake --build . --config Release
-cmake --build . --config Debug
+cmake --build . --config Release --target install
+cmake --build . --config Debug --target install
 popd
