570339a925acf2f497087a53faf9108df1835e6c
ShepherdOuO <2833244138@qq.com>
2022-10-01 23:50:58 +0800

    change .gitmodule

--- build_crossguid.bat
+++ build_crossguid.bat
@@ -3,6 +3,6 @@ git submodule update --init External\src\crossguid
 mkdir External\build\crossguid
 pushd External\build\crossguid
 cmake -S ..\..\src\crossguid -B .
-cmake --build . --config Release
-cmake --build . --config Debug
+cmake --build . --config Release --target install
+cmake --build . --config Debug --target install
 popd
